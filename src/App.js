1eb4385818fcbe0fb423807e2edc83d8957c2350
sulyman <kamaldeensulyman@gmail.com>
2022-02-22 18:32:22 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo-app'>
+      <a className="github-fork-ribbon right-top" href="https://twitter.com/Dev_suite" rel="noopener" data-ribbon="Follow Up On Twitter" title="Fork me on GitHub">Fork me on GitHub </a>
+      <TodoList />
     </div>
   );
 }
